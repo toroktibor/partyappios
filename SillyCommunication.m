e1a3c927956baf7b86eff5a9a514740ea75fe8d7
toroktibor <trktiti@gmail.com>
2014-01-20 15:42:38 +0100

    Kommunikációs.

--- SillyCommunication.m
+++ SillyCommunication.m
@@ -7,16 +7,58 @@
 //
 
 #import "SillyCommunication.h"
+#import "User.h"
+#import "MenuItem.h"
 
-@implementation SillyCommunication
+@implementation SillyCommunication{
+    NSMutableArray * users;
+    NSMutableArray * clubs;
+}
+
+- (id)init
+{
+    users = [[NSMutableArray alloc] init];
+    User * auser = [[User alloc] initWithId:0 andNickName:@"a" andPassword:@"a" andEmail:@"a@a.com" andSex:0 andBirthday:@"1990.01.01" andType:0];
+    [users addObject:auser];
+    User * buser = [[User alloc] initWithId:0 andNickName:@"b" andPassword:@"b" andEmail:@"b@b.com" andSex:0 andBirthday:@"1991.01.01" andType:0];
+    [users addObject:buser];
+    User * cuser = [[User alloc] initWithId:0 andNickName:@"c" andPassword:@"c" andEmail:@"c@c.com" andSex:0 andBirthday:@"1992.01.01" andType:1];
+    [users addObject:cuser];
+    return self;
+    
+    clubs = [[NSMutableArray alloc]init];
+    Club * aclub = [[Club alloc] initWithId:0 andName:@"F@sz Kivan" andType:@"Kocsma" andDescription:@"Kicsit ideges vagyok mostmár!!!" andAddress:@"Debrecen, Kishegyesi utca 49" andPhonenumber:@"06306665556" andEmail:@"isten@f.a" andDate:@"2000.01.01"];
+    Club * bclub = [[Club alloc] initWithId:1 andName:@"Bebasz-Lak" andType:@"Elithely" andDescription:@"Kicsit sem igényes hely." andAddress:@"Debrecen, Kishegyesi utca 50" andPhonenumber:@"06306665559" andEmail:@"isten@f.sza" andDate:@"2000.01.01"];
+    Club * cclub = [[Club alloc] initWithId:2 andName:@"Kivan" andType:@"Lokál" andDescription:@"Csúnya lesz!" andAddress:@"Budapest, Kishegyesi utca 56" andPhonenumber:@"06306365556" andEmail:@"isten@jozsi.a" andDate:@"2000.01.01"];
+    
+    NSMutableArray * itemArray = [[NSMutableArray alloc] init];
+    MenuItem * amenuItem = [[MenuItem alloc] initWithId:0 andName:@"Szar" andPrice:100 andCurrency:@"HUF" andUnit:@"liter" andDiscount:0 andMenuCategory:@"Desszert" andMenuSort:0];
+    MenuItem * bmenuItem = [[MenuItem alloc] initWithId:1 andName:@"Szar1" andPrice:1000 andCurrency:@"HUF" andUnit:@"liter" andDiscount:0 andMenuCategory:@"Desszert" andMenuSort:0];
+    MenuItem * cmenuItem = [[MenuItem alloc] initWithId:2 andName:@"Szar2" andPrice:500 andCurrency:@"HUF" andUnit:@"liter" andDiscount:0 andMenuCategory:@"Desszert" andMenuSort:0];
+    [itemArray addObject:amenuItem];
+    [itemArray addObject:bmenuItem];
+    [itemArray addObject:cmenuItem];
+    
+    [aclub setMenuItems:itemArray];
+    
+    [clubs addObject:aclub];
+    [clubs addObject:bclub];
+    [clubs addObject:cclub];
+    
+    
+}
 
 -(User *)authenticationUserWithNickName:(NSString *)nick_name andPassword:(NSString *)password {
     
-    if( [nick_name isEqualToString: @"a" ] && [password isEqualToString: @"a"]) {
-        return [[User alloc] initWithId: 0 andNickName:@"a" andPassword:@"a" andEmail:@"a@a.com" andSex:1 andBirthday: @"1991.01.01." andType:0 ];
+    
+    for (int i = 0; i<[users count]; i++) {
+        NSInteger index = i;
+        User * actualUser = [users objectAtIndex:index];
+        if( [nick_name isEqualToString: [actualUser getName] ] && [password isEqualToString: [actualUser getPassword]]) {
+            return actualUser;
+        }
     }
-    else
-        return nil;
+    return nil;
 }
 
 -(void)sendANewClubRequestWithClubname:(NSString *)newClubName andAddress:(NSString *)newClubAddress andType:(NSString *)newClubType andOwnerUserId:(NSInteger *)owner_user_id {
@@ -24,15 +66,33 @@
 }
 
 -(NSMutableArray *)getClubsFromCityName:(NSString *)cityname {
-    return nil;
+    NSMutableArray * out = [[NSMutableArray alloc]init];
+    for (int i = 0; i<[clubs count]; i++) {
+        NSInteger index = i;
+        User * actualClub = [clubs objectAtIndex:index];
+        if( [actualClub.getName hasPrefix:cityname]) {
+            [out addObject:actualClub];
+        }
+    }
+    return out;
 }
 
 -(NSMutableArray *)getFavoriteClubsFromUserId:(NSInteger *)user_id {
-    return nil;
+    NSMutableArray * out = [[NSMutableArray alloc]init];
+    if(user_id == 0){
+        [out addObject:[clubs objectAtIndex:0]];
+        [out addObject:[clubs objectAtIndex:1]];
+    }
+    return out;
 }
 
 -(NSMutableArray *)getOwnedClubsFromUserId:(NSInteger *)user_id{
-    return nil;
+    NSMutableArray * out = [[NSMutableArray alloc]init];
+    if(user_id == 0){
+        [out addObject:[clubs objectAtIndex:0]];
+        [out addObject:[clubs objectAtIndex:1]];
+    }
+    return out;
 }
 
 -(void)modifyPasswordWithId:(NSInteger *)id andPassword:(NSString *)password{
