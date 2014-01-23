3dd7a69ee645511281d8e466d1ff66e0d20721fb
vtms <vtamashun@gmail.com>
2014-01-23 15:58:41 +0100

    eltel ital

--- bcsapat/DrinkOrFoodView.h
+++ bcsapat/DrinkOrFoodView.h
@@ -9,5 +9,11 @@
 #import <UIKit/UIKit.h>
 
 @interface DrinkOrFoodView : UITableViewController
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *priceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *currencyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *unitLabel;
+@property (weak, nonatomic) IBOutlet UILabel *discountLabel;
 
 @end
