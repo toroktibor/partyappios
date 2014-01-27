9b6ccad2cde311e915082980f9f43e2bb27da9df
toroktibor <trktiti@gmail.com>
2014-01-27 20:23:49 +0100

    komunikáció

--- Communication.m
+++ Communication.m
@@ -10,50 +10,89 @@
 
 @implementation Communication
 
+NSString * mainUrl = @"http://partyapp.bugs3.com/";
+NSMutableData * responseData = nil;
 
+-(void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response{
+    responseData = [[NSMutableData alloc] init];
+}
 
--(User *) authenticationUserWithNickName:(NSString *) nick_name andPassword:(NSString*) password{
-//    1. set post string with actual username and password.    
-    NSString *post = [NSString stringWithFormat:@"&NickName=%@&Password=%@",@"nick_name",@"password"];
-    NSLog(@"%@",post);
-//    2. Encode the post string using NSASCIIStringEncoding and also the post string you need to send in NSData format.    
-    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+-(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data{
+    [responseData appendData:data];
+}
+
+-(void)connectionDidFinishLoading:(NSURLConnection *)connection{
+    NSLog(@"Letöltés kész");
+    NSLog(@"Data: %@", [[NSString alloc] initWithData:responseData encoding:NSUTF8StringEncoding]);
+}
+
+-(NSString *) httpPost: (NSString *) file withData: (NSMutableDictionary *) data{
+    NSString *url = [NSString stringWithFormat:@"%@%@", mainUrl, file ];
+    NSLog(@"HTTP POST");
+    NSLog(@"URL: %@", url);
     
-//    You need to send the actual length of your data. Calculate the length of the post string.    
+    NSMutableURLRequest * request = [[NSMutableURLRequest alloc] initWithURL:[[NSURL alloc] initWithString:url] ];
+    [request setHTTPMethod:@"POST"];
+    
+    
+    NSString *post = [NSString stringWithFormat:@"&NickName=%@&Password=%@",@"nick_name",@"password"];
+    NSLog(@"POST: %@", post);
+    NSData *postData = [post dataUsingEncoding:NSUTF8StringEncoding allowLossyConversion:YES];
     NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
     
-//    3. Create a Urlrequest with all the properties like HTTP method, http header field with length of the post string.
-//    Create URLRequest object and initialize it.    
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    
-//    Set the Url for which your going to send the data to that request.    
-        [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.schonherz.bl.ee/partyapp/user.php"]]];
-    
-//    Now, set HTTP method (POST or GET).
-//    Write this lines as it is in your code.    
-    [request setHTTPMethod:@"GET"];
-    
-//    Set HTTP header field with length of the post data.    
     [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
     
-//    Also set the Encoded value for HTTP header Field.    
-        [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
     
-//    Set the HTTPBody of the urlrequest with postData.    
     [request setHTTPBody:postData];
     
-//    4. Now, create URLConnection object. Initialize it with the URLRequest.    
+    NSURLConnection * conn= [[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    
+}
+
+-(User *) authenticationUserWithNickName:(NSString *) nick_name andPassword:(NSString*) password{
+    //    1. set post string with actual username and password.
+    NSString *post = [NSString stringWithFormat:@"&NickName=%@&Password=%@",@"nick_name",@"password"];
+    NSLog(@"%@",post);
+    //    2. Encode the post string using NSASCIIStringEncoding and also the post string you need to send in NSData format.
+    NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
+    
+    //    You need to send the actual length of your data. Calculate the length of the post string.
+    NSString *postLength = [NSString stringWithFormat:@"%d",[postData length]];
+    
+    //    3. Create a Urlrequest with all the properties like HTTP method, http header field with length of the post string.
+    //    Create URLRequest object and initialize it.
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
+    
+    //    Set the Url for which your going to send the data to that request.
+    [request setURL:[NSURL URLWithString:[NSString stringWithFormat:@"http://www.schonherz.bl.ee/partyapp/user.php"]]];
+    
+    //    Now, set HTTP method (POST or GET).
+    //    Write this lines as it is in your code.
+    [request setHTTPMethod:@"GET"];
+    
+    //    Set HTTP header field with length of the post data.
+    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
+    
+    //    Also set the Encoded value for HTTP header Field.
+    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
+    
+    //    Set the HTTPBody of the urlrequest with postData.
+    [request setHTTPBody:postData];
+    
+    //    4. Now, create URLConnection object. Initialize it with the URLRequest.
     NSURLConnection *conn = [[NSURLConnection alloc]initWithRequest:request delegate:self];
     
-//    It returns the initialized url connection and begins to load the data for the url request. You can check that whether you URL connection is done properly or not using just if/else statement as below.    
-        if(conn)
-        {
-            NSLog(@"Connection successful");
-        }
-        else
-        {
-            NSLog(@"Connection could not be made");
-        }
+    //    It returns the initialized url connection and begins to load the data for the url request. You can check that whether you URL connection is done properly or not using just if/else statement as below.
+    if(conn)
+    {
+        NSLog(@"Connection successful");
+    }
+    else
+    {
+        NSLog(@"Connection could not be made");
+    }
 }
 
 
