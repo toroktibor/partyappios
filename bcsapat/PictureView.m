89128fa2f9519e97af9b81f7d0e64ffa64d9d26d
user <user@users-Mac.local>
2014-01-29 13:45:41 -0800

    Lapozható galéria

--- bcsapat/PictureView.m
+++ bcsapat/PictureView.m
@@ -31,8 +31,8 @@
 	// Do any additional setup after loading the view.
     
     
-    image = [[Session getInstance]getImage];
-    [imageView setImage:image];
+    /*image = [[Session getInstance]getImage];
+    [imageView setImage:image];*/
     
     UIBarButtonItem *nextButton = [[UIBarButtonItem alloc] initWithTitle:@"Következő" style:UIBarButtonItemStyleBordered target:self action:@selector(next)];
     UIBarButtonItem *prevButton = [[UIBarButtonItem alloc] initWithTitle:@"Előző" style:UIBarButtonItemStyleBordered target:self action:@selector(back)];
@@ -63,15 +63,96 @@
     }
 }
 
+-(void)changeImage:(int)newid{
+    //int currentImageIndex = [[Session getInstance] getImage];
+    //int newImageIndex=currentImageIndex+1;
+    int newImageIndex=newid;
+    if( newImageIndex < 0 || newImageIndex > [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] count]-1){
+        return;
+    }
+    if( newImageIndex==0 ){
+        
+    }else if( newImageIndex== [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] count]-1 ){
+        
+    }
+    
+    [[Session getInstance] setImage:newImageIndex];
+    
+    GaleryImage * actualGaleryImage = [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] objectAtIndex:newImageIndex];
+    
+    if( actualGaleryImage.bitmap == nil)
+        [actualGaleryImage downloadBitmap];
+    
+    
+    [self.imageView setImage:[actualGaleryImage bitmap]];
+}
+
+-(void)slide:(int)position{
+    [ self changeImage: [[Session getInstance] getImage]+position ];
+}
 
 -(void)next{
+    [self slide:1];
+}
+
+
+
+-(void)back{
+    [self slide:-1];
+}
+
+
+/*-(void)next{
     NSLog(@"next");
+    
+    PictureView *PicturesDetailView=
+    [self.storyboard instantiateViewControllerWithIdentifier:@"PictureView"];
+    
+    int currentImageIndex = [[Session getInstance] getImage];
+    int newImageIndex=currentImageIndex+1;
+    if( newImageIndex < 0 || newImageIndex > [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] count]-1){
+        return;
+    }
+    [[Session getInstance] setImage:newImageIndex];
+    
+    GaleryImage * actualGaleryImage = [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] objectAtIndex:newImageIndex];
+        
+    if( actualGaleryImage.bitmap == nil)
+        [actualGaleryImage downloadBitmap];    
+    
+    
+    [self.navigationController pushViewController:PicturesDetailView animated:YES];    
+    [self.navigationController popViewControllerAnimated:true];
+    
+    [PicturesDetailView.imageView setImage:[actualGaleryImage bitmap]];
 }
 
 
 
 -(void)back{
     NSLog(@"back");
+    
+    PictureView *PicturesDetailView=
+    [self.storyboard instantiateViewControllerWithIdentifier:@"PictureView"];
+    
+    int currentImageIndex = [[Session getInstance] getImage];
+    int newImageIndex=currentImageIndex-1;
+    if( newImageIndex < 0 || newImageIndex > [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] count]-1){
+        return;
+    }
+    [[Session getInstance] setImage:newImageIndex];
+    
+    GaleryImage * actualGaleryImage = [[[[Session getInstance] getSelectedClubAtIndex: [[Session getInstance] getSelectedIndex]] getImages] objectAtIndex:newImageIndex];
+    
+    if( actualGaleryImage.bitmap == nil)
+        [actualGaleryImage downloadBitmap];
+    
+    
+    [self.navigationController pushViewController:PicturesDetailView animated:YES];
+    [self.navigationController popViewControllerAnimated:true];
+    
+    [PicturesDetailView.imageView setImage:[actualGaleryImage bitmap]];
 }
+ */
 
 @end
