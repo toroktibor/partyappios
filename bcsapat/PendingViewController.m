03794946f9ac3fdd88bc31614751b79a60b59667
toroktibor <trktiti@gmail.com>
2014-01-31 19:34:35 +0100

    Állat design.

--- bcsapat/PendingViewController.m
+++ bcsapat/PendingViewController.m
@@ -26,8 +26,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
-}
+    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"bricskok.png"]];	}
 
 - (void)didReceiveMemoryWarning
 {
